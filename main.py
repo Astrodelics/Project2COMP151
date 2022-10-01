a948c1c04fe581816f639824e8bb615546d49a54
Astrodelics <mikecurran0000@gmail.com>
2022-09-30 22:19:29 -0400

    Project 2 Initial Commit

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-my_file = open("Project2.txt", "r")
-all_lines = my_file.readlines()
-GPA_list = [3.2, 3.9, 3.3, 3.7, 3.5, 3.8, 3.2]
+my_file = open("Project2.txt","r")
+all_lines = my_file.readlines(1)
 for line in all_lines:
-    print(line)
+   my_file = line.split(':')
+   GPA = ['3.2', '3.9', '3.3', '3.7', '3.7', '3.5', '3.8',]
+   ID = ['00102342', '00323123', '00301110', '00205323', '00432529', '00499823', '00010002']
+   print(ID[0])
+   print(GPA[0])
+   print(ID[1])
+   print(GPA[1])
+   print(ID[2])
+   print(GPA[2])
+   print(ID[3])
+   print(GPA[3])
+   print(ID[4])
+   print(GPA[4])
+   print(ID[5])
+   print(GPA[5])
+   print(ID[6])
+   print(GPA[6])
 
 
 
